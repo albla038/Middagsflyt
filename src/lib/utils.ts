4c4b5324d348d11a6f645fa58c54110cd98f031a
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-06-26 09:03:19 +0200

    Generic slugify function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function removeAccents(str: string) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+export function slugify(...args: string[]) {
+  const joined = args.join("-").toLowerCase().trim();
+  const normalized = removeAccents(joined);
+
+  return normalized
+    .replace(/\s+/g, "-") // Replace spaces with hyphens
+    .replace(/[^a-z0-9-]/g, "") // Remove non-alphanumeric characters except hyphens
+    .replace(/-+/g, "-") // Replace multiple hyphens with a single hyphen
+    .replace(/^-+|-+$/g, ""); // Remove leading and trailing hyphens
 }
