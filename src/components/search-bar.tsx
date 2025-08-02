0e43afa321312c2e4118b7cf1439223207e9ce0d
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-08-02 19:03:01 +0200

    Added URL state for seach queries

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,10 +1,35 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import { ComponentPropsWithoutRef } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { ComponentPropsWithoutRef, useMemo } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 type SearhBarProps = ComponentPropsWithoutRef<"input">;
 
 export default function SearchBar({ ...rest }: SearhBarProps) {
-  return <Input {...rest} type="search"></Input>;
+  const params = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const searchParams = useMemo(() => new URLSearchParams(params), [params]);
+
+  const handleInput = useDebouncedCallback((key: string, input: string) => {
+    if (input) {
+      searchParams.set(key, input);
+    } else {
+      searchParams.delete(key);
+    }
+
+    router.replace(`${pathname}/?${searchParams.toString()}`);
+  }, 200);
+
+  return (
+    <Input
+      {...rest}
+      type="text"
+      defaultValue={searchParams.get("query")?.toString()}
+      onChange={(event) => handleInput("query", event.target.value)}
+    ></Input>
+  );
 }
