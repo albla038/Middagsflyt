004f5061a59a69914302d45ab8c02dc71a0741b4
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-08-14 10:00:43 +0200

    Added Schedule mutation functions

---
+++ src/data/schedule/mutations.ts
@@ -0,0 +1,104 @@
+import "server-only";
+
+import { requireHouseholdId } from "@/data/household/queries";
+import prisma from "@/lib/db";
+import { Result } from "@/lib/types";
+import { requireUser } from "@/data/user/verify-user";
+
+export async function createSchedule(
+  name: string,
+  description?: string,
+): Promise<Result<void, Error>> {
+  try {
+    const householdId = await requireHouseholdId();
+
+    await prisma.schedule.create({
+      data: {
+        name,
+        description,
+        householdId,
+      },
+    });
+
+    return {
+      ok: true,
+      data: undefined,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      error: new Error("Failed to create schedule", {
+        cause: error instanceof Error ? error : new Error(String(error)),
+      }),
+    };
+  }
+}
+
+export async function deleteSchedule(
+  scheduleId: string,
+): Promise<Result<void, Error>> {
+  const user = await requireUser();
+
+  try {
+    await prisma.schedule.delete({
+      where: {
+        household: {
+          members: {
+            some: { userId: user.id },
+          },
+        },
+        id: scheduleId,
+      },
+    });
+
+    return {
+      ok: true,
+      data: undefined,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      error: new Error(`Failed to delete schedule with ID: ${scheduleId}`, {
+        cause: error instanceof Error ? error : new Error(String(error)),
+      }),
+    };
+  }
+}
+
+export async function renameSchedule(
+  scheduleId: string,
+  newName: string,
+): Promise<Result<string, Error>> {
+  const user = await requireUser();
+
+  try {
+    const result = await prisma.schedule.update({
+      where: {
+        household: {
+          members: {
+            some: { userId: user.id },
+          },
+        },
+        id: scheduleId,
+      },
+      data: {
+        name: newName,
+      },
+      select: {
+        name: true,
+      },
+    });
+
+    return {
+      ok: true,
+      data: result.name,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      error: new Error(`Failed to rename schedule with ID: ${scheduleId}`, {
+        cause: error instanceof Error ? error : new Error(String(error)),
+      }),
+    };
+  }
+}
