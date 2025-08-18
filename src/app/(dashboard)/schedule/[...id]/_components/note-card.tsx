73b8a5869d7d496c61ec0d51b6adce9dad6b4086
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-08-18 17:42:09 +0200

    Scheduled note card markup and styling

---
+++ src/app/(dashboard)/schedule/[...id]/_components/note-card.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import SaveNoteDialog, {
+  SaveNoteDialogState,
+} from "@/app/(dashboard)/schedule/[...id]/_components/save-note-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ScheduledNote } from "@/lib/generated/prisma";
+import { Edit, MoreHorizontal, Trash2 } from "lucide-react";
+
+type NoteCardProps = {
+  note: ScheduledNote;
+};
+
+export default function NoteCard({ note }: NoteCardProps) {
+
+  return (
+    <article className="flex flex-col gap-2 rounded-md border border-border p-3">
+      <div className="flex flex-col gap-1">
+        <div className="flex items-start justify-between text-sm">
+          <h3 className="font-medium text-pretty">{note.title}</h3>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className="size-5">
+                <MoreHorizontal className="size-full" />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="start" side="right" sideOffset={8}>
+              <DropdownMenuGroup>
+                <DropdownMenuItem
+                >
+                  <Edit />
+                  Redigera
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <Trash2 className="text-destructive" />
+                  Ta bort
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem disabled>
+                  Skapad{" "}
+                  {note.createdAt.toLocaleString("sv-SE", {
+                    dateStyle: "medium",
+                  })}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <p className="text-xs wrap-normal text-muted-foreground">{note.text}</p>
+      </div>
+
+      <SaveNoteDialog
+        dialogState={saveDialogState}
+        setDialogState={setSaveDialogState}
+      />
+    </article>
+  );
+}
