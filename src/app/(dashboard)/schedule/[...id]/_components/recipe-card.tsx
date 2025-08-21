76d2a0660445f032729501d40420367d582c83cf
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-08-21 11:43:44 +0200

    Added dropdown menu, Recipe link, and note to <RecipeCard>

--- src/app/(dashboard)/schedule/[...id]/_components/recipe-card.tsx
+++ src/app/(dashboard)/schedule/[...id]/_components/recipe-card.tsx
@@ -4,19 +4,44 @@ import StatValueSmall from "@/components/stat-value-small";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Recipe, ScheduledRecipe } from "@/lib/generated/prisma";
-import { ClockFading, MoreHorizontal, Utensils } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ScheduledRecipeDisplayContent } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  ClockFading,
+  Edit,
+  MoreHorizontal,
+  Notebook,
+  Trash2,
+  Utensils,
+} from "lucide-react";
+import Link from "next/link";
 
 type RecipeCardProps = {
-  scheduledRecipe: ScheduledRecipe & {
-    recipe: Recipe;
-  };
+  scheduledRecipe: ScheduledRecipeDisplayContent;
 };
 
 export default function RecipeCard({ scheduledRecipe }: RecipeCardProps) {
-  const { id: scheduledRecipeId, recipe, servings } = scheduledRecipe;
+  const {
+    id: scheduledRecipeId,
+    date,
+    servings,
+    note,
+    recipe,
+    assignee,
+    createdAt,
+  } = scheduledRecipe;
+
   const {
     id: recipeId,
+    slug,
     name,
     totalTimeSeconds,
     proteinType,
@@ -24,19 +49,63 @@ export default function RecipeCard({ scheduledRecipe }: RecipeCardProps) {
   } = recipe;
 
   return (
-    <article className="flex flex-col gap-2 rounded-md bg-subtle p-3">
+    <article
+      className={cn(
+        "flex flex-col gap-2 rounded-md border border-subtle bg-subtle p-3",
+        "has-[[aria-checked=true]]:border-primary has-[[aria-checked=true]]:bg-primary-foreground",
+      )}
+    >
       {/* Action buttons */}
       <div className="flex items-center justify-between">
         <Checkbox />
-        <Button variant="ghost" size="icon" className="size-5">
-          <MoreHorizontal className="size-full" />
-        </Button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="size-5">
+              <MoreHorizontal className="size-full" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="start" side="right" sideOffset={8}>
+            <DropdownMenuGroup>
+              {/* // TODO Add action */}
+              <DropdownMenuItem>
+                <Edit />
+                Redigera
+              </DropdownMenuItem>
+
+              {/* // TODO Add action */}
+              <DropdownMenuItem>
+                <Trash2 className="text-destructive" />
+                Ta bort
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem disabled>
+                Skapad{" "}
+                {createdAt.toLocaleString("sv-SE", {
+                  dateStyle: "medium",
+                })}
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       <div className="flex flex-col gap-2">
-        <h3 className="line-clamp-2 text-sm font-medium tracking-tight text-pretty">
-          {name}
-        </h3>
+        <Link
+          href={`/saved-recipes/${slug}`} // TODO Replace with /[schedule]/[week]/[recipeSlug]
+          className={"hover:underline"}
+        >
+          <h3
+            className={
+              "line-clamp-2 text-base font-medium tracking-tight text-pretty"
+            }
+          >
+            {name}
+          </h3>
+        </Link>
 
         <div className="flex flex-wrap items-center gap-2">
           {totalTimeSeconds && (
@@ -49,13 +118,22 @@ export default function RecipeCard({ scheduledRecipe }: RecipeCardProps) {
           )}
         </div>
 
+        {note && (
+          <div className="flex gap-1 text-xs text-muted-foreground">
+            <span className="flex h-[1lh] items-center">
+              <Notebook className="size-3" />
+            </span>
+            <p>{note}</p>
+          </div>
+        )}
+
         <div className="flex flex-wrap items-center gap-2">
           {proteinType && (
-            <Badge variant="outline" className="text-[10px] px-1.5">
+            <Badge variant="outline">
               {proteinType.charAt(0) + proteinType.slice(1).toLowerCase()}
             </Badge>
           )}
-          <Badge variant="outline" className="text-[10px] px-1.5">
+          <Badge variant="outline">
             {recipeType.charAt(0) + recipeType.slice(1).toLowerCase()}
           </Badge>
         </div>
