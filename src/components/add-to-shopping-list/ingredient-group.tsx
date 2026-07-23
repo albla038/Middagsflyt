061fd75780e435abfdbe9a6ee3f0b2377bba67b0
Albert Lännerström <albert.lannerstrom@gmail.com>
2026-07-23 14:05:41 +0200

    fix: Adjust font leading and spacing in ingredient group and skeleton components

--- src/components/add-to-shopping-list/ingredient-group.tsx
+++ src/components/add-to-shopping-list/ingredient-group.tsx
@@ -36,24 +36,22 @@ export default function IngredientGroup({
 
   return (
     <Collapsible className="group/collapsible">
-      <div className="space-y-2 p-1">
-        <div className="flex w-full items-center gap-2">
-          <Checkbox
-            checked={checkedState}
-            onCheckedChange={(checked) => {
-              if (typeof checked === "boolean")
-                onToggleGroup(checked, recipe.sourceId);
-            }}
-          />
-          <CollapsibleTrigger className="flex w-full cursor-pointer items-center justify-between">
-            <span className="line-clamp-1 grow text-left leading-none font-medium">
-              {recipe.name}
-            </span>
-            <ChevronRight className="size-4 shrink-0 transition-transform group-data-[state=open]/collapsible:rotate-90" />
-          </CollapsibleTrigger>
-        </div>
-        <CollapsibleContent className="pl-6">{children}</CollapsibleContent>
+      <div className="flex w-full items-center gap-2">
+        <Checkbox
+          checked={checkedState}
+          onCheckedChange={(checked) => {
+            if (typeof checked === "boolean")
+              onToggleGroup(checked, recipe.sourceId);
+          }}
+        />
+        <CollapsibleTrigger className="flex w-full cursor-pointer items-center justify-between gap-2">
+          <span className="line-clamp-1 grow text-left font-medium">
+            {recipe.name}
+          </span>
+          <ChevronRight className="size-4 shrink-0 transition-transform group-data-[state=open]/collapsible:rotate-90" />
+        </CollapsibleTrigger>
       </div>
+      <CollapsibleContent className="pl-6">{children}</CollapsibleContent>
     </Collapsible>
   );
 }
--- src/components/add-to-shopping-list/skeletons/ingredient.tsx
+++ src/components/add-to-shopping-list/skeletons/ingredient.tsx
@@ -4,33 +4,27 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function IngredientSkeleton() {
   return (
     <div className="flex flex-col gap-4">
-      <div className="mb-2 space-y-2 p-1">
-        <div className="flex w-full items-center gap-2">
-          <Checkbox disabled />
-          <div className="flex w-full cursor-pointer items-center justify-between">
-            <Skeleton className="h-4 w-56"></Skeleton>
-            <Skeleton className="size-4 rounded-sm" />
-          </div>
+      <div className="flex w-full items-center gap-2">
+        <Checkbox disabled />
+        <div className="flex w-full cursor-pointer items-center justify-between">
+          <Skeleton className="my-1 h-4 w-56" />
+          <Skeleton className="size-4 rounded-sm" />
         </div>
       </div>
 
-      <div className="mb-2 space-y-2 p-1">
-        <div className="flex w-full items-center gap-2">
-          <Checkbox disabled />
-          <div className="flex w-full cursor-pointer items-center justify-between">
-            <Skeleton className="h-4 w-72"></Skeleton>
-            <Skeleton className="size-4 rounded-sm" />
-          </div>
+      <div className="flex w-full items-center gap-2">
+        <Checkbox disabled />
+        <div className="flex w-full items-center justify-between">
+          <Skeleton className="my-1 h-4 w-72" />
+          <Skeleton className="size-4 rounded-sm" />
         </div>
       </div>
 
-      <div className="mb-2 space-y-2 p-1">
-        <div className="flex w-full items-center gap-2">
-          <Checkbox disabled />
-          <div className="flex w-full cursor-pointer items-center justify-between">
-            <Skeleton className="h-4 w-48"></Skeleton>
-            <Skeleton className="size-4 rounded-sm" />
-          </div>
+      <div className="flex w-full items-center gap-2">
+        <Checkbox disabled />
+        <div className="flex w-full items-center justify-between">
+          <Skeleton className="my-1 h-4 w-48" />
+          <Skeleton className="size-4 rounded-sm" />
         </div>
       </div>
     </div>
