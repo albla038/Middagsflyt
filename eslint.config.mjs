dddb9850ad75197aa0ce855b09021c958b9edfc5
Albert Lännerström <albert.lannerstrom@gmail.com>
2026-07-27 08:54:07 +0200

    build: Fix ESLint config and version issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,23 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = [
-  ...nextCoreWebVitals,
-  ...nextTypescript,
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
   {
     rules: {
-      "no-unused-vars": "warn",
       "prefer-const": "warn",
     },
   },
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
 export default eslintConfig;
