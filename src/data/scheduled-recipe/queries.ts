207e418d75005866fde73d17004318ccc30c1170
Albert Lännerström <albert.lannerstrom@gmail.com>
2026-08-11 10:02:47 +0200

    feat: Add DAL query to fetch scheduled recipe with isSaved property

--- src/data/scheduled-recipe/queries.ts
+++ src/data/scheduled-recipe/queries.ts
@@ -3,6 +3,87 @@ import "server-only";
 import { requireUser } from "@/data/user/verify-user";
 import prisma from "@/lib/db";
 import { ScheduledRecipeDisplayContent } from "@/lib/types";
+import { recipeSelects } from "@/data/recipe/queries";
+import { Recipe } from "@/lib/types/recipe";
+
+export async function fetchScheduledRecipe(id: string): Promise<{
+  id: string;
+  date: Date;
+  servings: number | null;
+  schedule: {
+    id: string;
+    name: string;
+  };
+  recipe: Recipe & { isSaved: boolean };
+} | null> {
+  const user = await requireUser();
+
+  const scheduledRecipe = await prisma.scheduledRecipe.findUnique({
+    where: {
+      id,
+
+      schedule: {
+        household: {
+          members: {
+            some: { userId: user.id },
+          },
+        },
+      },
+    },
+
+    select: {
+      id: true,
+      date: true,
+      servings: true,
+
+      schedule: {
+        select: { id: true, name: true },
+      },
+
+      recipe: {
+        select: {
+          ...recipeSelects,
+
+          _count: {
+            select: {
+              savedBy: {
+                where: {
+                  household: {
+                    members: {
+                      some: { userId: user.id },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!scheduledRecipe) return null;
+
+  // Transform the nested recipeIngredients to be a string array
+  const transformedInstructions = scheduledRecipe.recipe.recipeInstructions.map(
+    (instruction) => ({
+      ...instruction,
+      recipeIngredients: instruction.recipeIngredients.map((ing) => ing.id),
+    }),
+  );
+
+  const { recipe, ...rest } = scheduledRecipe;
+  const { _count, ...recipeRest } = recipe;
+
+  return {
+    ...rest,
+    recipe: {
+      ...recipeRest,
+      isSaved: _count.savedBy > 0,
+      recipeInstructions: transformedInstructions,
+    },
+  };
+}
 
 export async function fetchScheduledRecipesByDateRange(
   scheduleId: string,
